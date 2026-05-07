b23b10f92466dee47a95059f035eb7039884a6ca
Isaac (Tsz Lai) Ng <103465188+caasion@users.noreply.github.com>
2026-05-07 11:31:51 -0400

    arch: Types for GET tags request

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,33 @@
+use serde::{Deserialize, Serialize};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TagsResponse {
+    pub models: Vec<Model>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Model {
+    pub name: String,
+    pub model: String,
+    pub modified_at: String,
+    pub size: u64,
+    pub digest: String,
+    pub details: ModelDetails,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ModelDetails {
+    pub format: String,
+    pub family: String,
+    pub families: Vec<String>,
+    pub parameter_size: String,
+    pub quantization_level: String,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
