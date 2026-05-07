c49bd9b13f51c7deeb7528b58c7997931dbeb5ab
Isaac (Tsz Lai) Ng <103465188+caasion@users.noreply.github.com>
2026-05-07 18:17:26 -0400

    chore: Remove greet function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -24,12 +24,6 @@ pub struct ModelDetails {
     pub quantization_level: String,
 }
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[tauri::command]
 async fn get_models() -> Result<Vec<Model>, String> {
     let client = reqwest::Client::new();
